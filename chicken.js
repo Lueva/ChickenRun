c3ece24027babdd2f43a2f9d49efadc7f67a9510
masallandre <maeva.sallandre2@soprasteria.com>
2021-04-29 12:05:23 +0200

    Manage error cases.

--- chicken.js
+++ chicken.js
@@ -1,5 +1,6 @@
 // Import chicken model
 Chicken = require('./chickenModel');
+mongoose = require('mongoose');
 
 // Get all chickens
 exports.index = function (req, res) {
@@ -20,105 +21,142 @@ exports.index = function (req, res) {
 
 // Get a specific chicken
 exports.get = function (req, res) {
-    Chicken.findById(req.params.chicken_id, function (err, chicken) {
-        if (err) {
-            res.send(err);
-        }
-        res.json({
-            message: 'Chicken information',
-            data: chicken
+    if (mongoose.Types.ObjectId.isValid(req.params.chicken_id)) {
+        Chicken.findById(req.params.chicken_id, function (err, chicken) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({
+                message: 'Chicken information',
+                data: chicken
+            });
         });
-    });
+    } else {
+        res.json({
+            status: "fail",
+            message: 'This chicken does not exists.'
+        });
+    }
 };
 
 // Create new chicken actions
 exports.new = function (req, res) {
-    var chicken = new Chicken();
-    chicken.name = req.body.name;
-    chicken.birthday = req.body.birthday;
-    chicken.weight = req.body.weight;
-    chicken.steps = 0;
-    chicken.isRunning = false;
-    // save the new chicken
-    chicken.save(function (err) {
-        if (err) {
-            res.json(err);
-        }
-        res.json({
-            message: 'A chicken is born!',
-            data: chicken
+    if (req.body.name && req.body.weight) {
+        var chicken = new Chicken();
+        chicken.name = req.body.name;
+        chicken.birthday = req.body.birthday;
+        chicken.weight = req.body.weight;
+        chicken.steps = 0;
+        chicken.isRunning = false;
+        // save the new chicken
+        chicken.save(function (err) {
+            if (err) {
+                res.json(err);
+            }
+            res.json({
+                message: 'A chicken is born!',
+                data: chicken
+            });
         });
-    });
+    } else {
+        res.json({
+            status: "Fail",
+            message: 'You need to give your chicken a name & a weight.',
+        });
+    }
 };
 
 // Handle update chicken info
 exports.update = function (req, res) {
-    Chicken.findById(req.params.chicken_id, function (err, chicken) {
-        if (err) {
-            res.send(err);
-        }
-        if (chicken) {
-            chicken.name = req.body.name;
-            chicken.birthday = req.body.birthday;
-            chicken.weight = req.body.weight;
-            chicken.steps = req.body.steps;
-            chicken.isRunning = req.body.isRunning;
-            // save the chicken and check for errors
-            chicken.save(function (err) {
-                if (err) {
-                    res.json(err);
-                }
-                res.json({
-                    message: 'Your chicken information has been updated.',
-                    data: chicken
+    if (mongoose.Types.ObjectId.isValid(req.params.chicken_id)) {
+        Chicken.findById(req.params.chicken_id, function (err, chicken) {
+            if (err) {
+                res.send(err);
+            }
+            if (chicken) {
+                chicken.name = req.body.name ? req.body.name : chicken.name;
+                chicken.birthday = req.body.birthday;
+                chicken.weight = req.body.weight ? req.body.weight : chicken.weight ;
+                chicken.steps = req.body.steps;
+                chicken.isRunning = req.body.isRunning;
+                // save the chicken and check for errors
+                chicken.save(function (err) {
+                    if (err) {
+                        res.json(err);
+                    }
+                    res.json({
+                        message: 'Your chicken information has been updated.',
+                        data: chicken
+                    });
                 });
-            });
-        } else {
-            res.json({
-                message: 'Your chicken is not born yet!',
-            });
-        }
-    });
+            } else {
+                res.json({
+                    status: "fail",
+                    message: 'This chicken is not born yet!',
+                });
+            }
+        });
+    } else {
+        res.json({
+            status: "fail",
+            message: 'This chicken does not exists.'
+        });
+    }
 };
 
 // Handle update chicken run info
 exports.run = function (req, res) {
-    Chicken.findById(req.params.chicken_id, function (err, chicken) {
-        if (err) {
-            res.send(err);
-        }
-        if (chicken) {
-            chicken.steps = chicken.steps + 1;
-            chicken.isRunning = true;
-            // save the chicken and check for errors
-            chicken.save(function (err) {
-                if (err) {
-                    res.json(err);
-                }
-                res.json({
-                    message: 'Your chicken is running!',
-                    data: chicken
+    if (mongoose.Types.ObjectId.isValid(req.params.chicken_id)) {
+        Chicken.findById(req.params.chicken_id, function (err, chicken) {
+            if (err) {
+                res.send(err);
+            }
+            if (chicken) {
+                chicken.steps = chicken.steps + 1;
+                chicken.isRunning = true;
+                // save the chicken and check for errors
+                chicken.save(function (err) {
+                    if (err) {
+                        res.json(err);
+                    }
+                    res.json({
+                        message: 'Your chicken is running!',
+                        data: chicken
+                    });
                 });
-            });
-        } else {
-            res.json({
-                message: 'Your chicken is not born yet!',
-            });
-        }
-    });
+            } else {
+                res.json({
+                    status: "fail",
+                    message: 'Your chicken is not born yet!',
+                });
+            }
+        });
+    } else {
+        res.json({
+            status: "fail",
+            message: 'This chicken does not exists.'
+        });
+    }
 };
 
 // Handle delete chicken 
 exports.delete = function (req, res) {
-    Chicken.remove({
-        _id: req.params.chicken_id
-    }, function (err, chicken) {
-        if (err) {
-            res.send(err);
-        }
-        res.json({
-            status: "success",
-            message: 'Your chicken has left. Be careful! '
+    if (mongoose.Types.ObjectId.isValid(req.params.chicken_id)) {
+        Chicken.remove({
+            _id: req.params.chicken_id
+        }, function (err, chicken) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({
+                status: "success",
+                message: 'Your chicken has left. Be careful! '
+            });
         });
-    });
+    } else {
+        res.json({
+            status: "fail",
+            message: 'This chicken does not exists.'
+        });
+    }
 };
--- farmyard.js
+++ farmyard.js
@@ -1,6 +1,7 @@
 // Import farmyard model
 Farmyard = require('./farmyardModel');
 Chicken = require('./chickenModel');
+mongoose = require('mongoose');
 
 // Get all chickens in the farmyard
 exports.index = function (req, res) {
@@ -21,47 +22,55 @@ exports.index = function (req, res) {
 
 // Add chicken to the farmyard
 exports.add = function (req, res) {
-    Chicken.findById(req.params.chicken_id, function (err, chicken) {
-        if (err) {
-            res.send(err);
-        }
-        if (chicken) {
-            Farmyard.find(function (err, farmyard) {
-                if (err) {
-                    res.send(err);
-                }
-                if (farmyard.length === 0) {
-                    farmyard = new Farmyard();
-                    farmyard.name = "My wonderful Farmyard";
-                    farmyard.chickens = [];
-                } else {
-                    farmyard = farmyard[0];
-                }
-                const chickenExists = farmyard.chickens.find(c => c === chicken._id);
-                if (chickenExists) {
-                    res.json({
-                        message: 'This chicken is already in your farmyard.',
-                        data: farmyard
-                    });
-                } else {
-                    farmyard.chickens.push(chicken._id);
-                }
-
-                // save farmyard
-                farmyard.save(function (err) {
+    if (mongoose.Types.ObjectId.isValid(req.params.chicken_id)) {
+        Chicken.findById(req.params.chicken_id, function (err, chicken) {
+            if (err) {
+                res.send(err);
+            }
+            if (chicken) {
+                Farmyard.find(function (err, farmyard) {
                     if (err) {
-                        res.json(err);
+                        res.send(err);
                     }
-                    res.json({
-                        message: 'A chicken arrived in your farmyard',
-                        data: farmyard
+                    if (farmyard.length === 0) {
+                        farmyard = new Farmyard();
+                        farmyard.name = "My wonderful Farmyard";
+                        farmyard.chickens = [];
+                    } else {
+                        farmyard = farmyard[0];
+                    }
+                    const chickenExists = farmyard.chickens.find(c => c === chicken._id);
+                    if (chickenExists) {
+                        res.json({
+                            message: 'This chicken is already in your farmyard.',
+                            data: farmyard
+                        });
+                    } else {
+                        farmyard.chickens.push(chicken._id);
+                    }
+
+                    // save farmyard
+                    farmyard.save(function (err) {
+                        if (err) {
+                            res.json(err);
+                        }
+                        res.json({
+                            message: 'A chicken arrived in your farmyard',
+                            data: farmyard
+                        });
                     });
                 });
-            });
-        } else {
-            res.json({
-                message: 'This chicken is not borned yet! You can\'t add it to the farmyard.'
-            });
-        }
-    });
+            } else {
+                res.json({
+                    status: "fail",
+                    message: 'This chicken is not borned yet! You can\'t add it to the farmyard.'
+                });
+            }
+        });
+    } else {
+        res.json({
+            status: "fail",
+            message: 'This chicken is not borned yet! You can\'t add it to the farmyard.'
+        });
+    }
 };
